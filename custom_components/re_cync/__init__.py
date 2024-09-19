562efcbf37be47939a089c41038f33345ffdbaaa
Kyle VanderBeek <kylev@kylev.com>
2024-09-19 01:30:27 +0000

    Go back to cloud

--- custom_components/re_cync/__init__.py
+++ custom_components/re_cync/__init__.py
@@ -4,14 +4,14 @@ from __future__ import annotations
 
 import logging
 
-from homeassistant.components import bluetooth
+# from homeassistant.components import bluetooth
 from homeassistant.config_entries import ConfigEntry, ConfigEntryAuthFailed
 from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
 
-# from .coordinator import AuthError, ReCyncCoordinator
-from .bt_coordinator import ReCyncBTCoordinator
+# from .bt_coordinator import ReCyncBTCoordinator
 from .const import DOMAIN
+from .coordinator import AuthError, ReCyncCoordinator
 
 _LOGGER = logging.getLogger(__name__)
 PLATFORMS: list[Platform] = [Platform.LIGHT, Platform.SWITCH]
@@ -22,13 +22,13 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     _LOGGER.debug("Setup entry %s", entry.data)
     hass.data.setdefault(DOMAIN, {})
 
-    # hub = ReCyncCoordinator(hass, entry)
-    # hass.data[entry.entry_id] = hub
+    hub = ReCyncCoordinator(hass, entry)
+    hass.data[entry.entry_id] = hub
 
-    # try:
-    #     await hub.start_cloud()
-    # except AuthError as err:
-    #     raise ConfigEntryAuthFailed("Authentication failed or expired") from err
+    try:
+        await hub.start_cloud()
+    except AuthError as err:
+        raise ConfigEntryAuthFailed("Authentication failed or expired") from err
 
     # def async_bluetooth_device_discovered(device: bluetooth.Device):
     #     """Handle a discovered Bluetooth device."""
@@ -38,10 +38,10 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     #     bluetooth.async_register_callback(hass, async_bluetooth_device_discovered)
     # )
 
-    coordinator = ReCyncBTCoordinator(hass, entry)
-    hass.data[DOMAIN][entry.entry_id] = coordinator
+    # coordinator = ReCyncBTCoordinator(hass, entry)
+    # hass.data[DOMAIN][entry.entry_id] = coordinator
 
-    # await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
     return True
 
